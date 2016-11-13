96b25af3c202d401e0db861a31e0753ab91157d9
chutipon29301 <chutiponh@hotmail.com>
2016-11-13 15:32:29 +0700

    Add license

--- src/DetailInput.java
+++ src/DetailInput.java
@@ -1,3 +1,17 @@
+//Copyright [2016] [Chutipon]
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -5,12 +19,23 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class DetailInput {
 
 	private static DetailInput window;
 	private JFrame frame;
 	private JLabel lblStudentId, lblSubject;
+	private JTextField textField;
+	private JButton btnCancel;
+	private String inputTextbox = "";
+	private JButton btnClear;
+	private JButton btnPrint;
 
 	/**
 	 * Launch the application.
@@ -18,7 +43,6 @@ public class DetailInput {
 	public static void main(String[] args) {
 		window = new DetailInput();
 		try {
-			System.out.println(args[0]);
 			window.lblStudentId.setText("Student ID: " + args[0].substring(0, args[0].length() - 1));
 			window.lblSubject.setText("Subject: " + ((args[0].charAt(args[0].length() - 1) == '1') ? "Math" : "Physics"));
 		} catch (Exception e1) {
@@ -31,15 +55,6 @@ public class DetailInput {
 			public void run() {
 				try {
 					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Index.main(null);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -70,7 +85,45 @@ public class DetailInput {
 		
 		lblSubject = new JLabel("Subject");
 		lblSubject.setFont(new Font("Cordia New", Font.PLAIN, 72));
-		lblSubject.setBounds(99, 139, 471, 84);
+		lblSubject.setBounds(99, 165, 471, 84);
 		frame.getContentPane().add(lblSubject);
+		
+		textField = new JTextField();
+		textField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				char charInput = arg0.getKeyChar();
+				if(charInput == '\n'){
+				}else {
+					inputTextbox += charInput;
+				}
+			}
+		});
+		textField.setBounds(99, 373, 1036, 126);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				window.frame.setVisible(false);
+				Index.main(null);
+			}
+		});
+		btnCancel.setBounds(1466, 754, 227, 84);
+		frame.getContentPane().add(btnCancel);
+		
+		btnClear = new JButton("Clear");
+		btnClear.setBounds(1114, 754, 227, 84);
+		frame.getContentPane().add(btnClear);
+		
+		btnPrint = new JButton("Print");
+		btnPrint.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println(inputTextbox);
+			}
+		});
+		btnPrint.setBounds(99, 754, 227, 89);
+		frame.getContentPane().add(btnPrint);
 	}
 }
