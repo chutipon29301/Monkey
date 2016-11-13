9e9340fab596c5e797655895f15d68ca664ae846
chutipon29301 <chutiponh@hotmail.com>
2016-11-13 13:10:35 +0700

    Fix window apperance setting label 1 and 2

--- src/DetailInput.java
+++ src/DetailInput.java
@@ -1,20 +1,45 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
 
 public class DetailInput {
 
+	private static DetailInput window;
 	private JFrame frame;
+	private JLabel lblStudentId, lblSubject;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		window = new DetailInput();
+		try {
+			System.out.println(args[0]);
+			window.lblStudentId.setText("Student ID: " + args[0].substring(0, args[0].length() - 1));
+			window.lblSubject.setText("Subject: " + ((args[0].charAt(args[0].length() - 1) == '1') ? "Math" : "Physics"));
+		} catch (Exception e1) {
+			JOptionPane.showMessageDialog(new JFrame(), "ID decrypt error return to Home");
+			e1.printStackTrace();
+			Index.main(null);
+			return;
+		}
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					DetailInput window = new DetailInput();
 					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Index.main(null);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -36,6 +61,16 @@ public class DetailInput {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 1920, 1080);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		lblStudentId = new JLabel("Student ID: ");
+		lblStudentId.setFont(new Font("Cordia New", Font.PLAIN, 72));
+		lblStudentId.setBounds(99, 55, 484, 62);
+		frame.getContentPane().add(lblStudentId);
+		
+		lblSubject = new JLabel("Subject");
+		lblSubject.setFont(new Font("Cordia New", Font.PLAIN, 72));
+		lblSubject.setBounds(99, 139, 471, 84);
+		frame.getContentPane().add(lblSubject);
 	}
-
 }
